f1e0c19f1fe31c3eea21f5a124a2347898ae2c34
Nakawukasandra <143192495+Nakawukasandra@users.noreply.github.com>
2023-10-19 10:30:40 +0300

    test

---
+++ test1.py
@@ -0,0 +1,23 @@
+#1.Given the following variable, rewrite them , in both snake case and camel case naming conventions.
+student_marks = (50,60,70,80,90)
+total_Average = 50+60+70+80+90
+print(total_Average)
+
+# 3.(i) Given alist, student_marks = [78,83,90,88,75]. write a python program to sum all the items in the list and the output as "The sum of the items in the student marks list is..."
+student_marks=[78,83,90,88,75]
+total=sum[student_marks]
+print(total)
+
+#(ii) display the first and last mark from the above list
+first_mark=78
+last_mark=75
+
+# (iii) Add 96 to th list
+student_marks=[78,83,90,88,75]
+student_marks.append(96)
+print(student_marks)
+
+#update the student mark of 78 t0 87
+
+
+  
